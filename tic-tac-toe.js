032e25a8c561f43f70e173e200699dc43a62d3f7
hjoel-code <hjoel.global@gmail.com>
2021-10-07 08:39:49 -0500

    Add JS file

---
+++ tic-tac-toe.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    var board = document.getElementById('board');
+    var squares = Array.from(board.children);
+
+   squares.forEach(node => {
+        node.classList.add('square')
+    });
+
+
+    var state = true
+
+    board.addEventListener('click', e => {
+        if (e.target.innerHTML == '') {
+            if (state) {
+                e.target.innerHTML = 'X'
+                e.target.classList.add('X')
+                state = false
+            } else {
+                e.target.innerHTML = 'O'
+                e.target.classList.add('O')
+                state = true
+            }
+        }
+    });
+
+    
+});
